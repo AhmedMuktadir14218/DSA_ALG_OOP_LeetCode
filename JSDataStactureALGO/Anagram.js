bdf4b44338b0b3b8a85ed9a45823c6aaec00f85b
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-10-30 21:52:52 +0600

    add demo

---
+++ JSDataStactureALGO/Anagram.js
@@ -0,0 +1,27 @@
+
+//String Anagram
+//"hello" -> "llehoo"
+
+const Anagram = (str1,str2) =>{
+    if (str1.length != str2.length) {
+        return false
+    }
+    let counter = {}
+    for(let i of str1){
+        // counter[i] = (counter[i] || 0) +1
+        counter[i] = (counter[i] || 0) +1
+        // console.log(counter[i])
+    }
+    for(let j of str2){
+        if (!counter[j]) {
+            return false
+        }
+        counter[j]-=1
+        console.log(counter[j])
+        
+    }
+    return true
+}
+
+const strr = Anagram('hello','lleho')
+console.log(strr)

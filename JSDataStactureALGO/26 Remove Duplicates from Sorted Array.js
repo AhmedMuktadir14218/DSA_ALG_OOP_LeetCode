280db119d877a3a1989bbac6369deb5c064b7b75
AhmedMuktadirUdoy <100287308+AhmedMuktadir14218@users.noreply.github.com>
2023-12-06 00:06:25 +0600

    Update 26 Remove Duplicates from Sorted Array.js

--- JSDataStactureALGO/26 Remove Duplicates from Sorted Array.js
+++ JSDataStactureALGO/26 Remove Duplicates from Sorted Array.js
@@ -1,3 +1,4 @@
+//O(n)
 nums =[0,1,2,2,2,2,3,5]
 var removeDuplicates = function(nums) {
     if (nums.length === 0) {
@@ -20,3 +21,24 @@ var removeDuplicates = function(nums) {
 console.log(removeDuplicates(nums))
 
 
+//O(n^2)
+const value =[0,0,1,1,2,2,3,3,4]
+
+const x=(array)=>{
+    let count=0;
+    let newArray=[]
+    for(let i =0; i<=array.length; i++){
+       // console.log(i)
+        for(let j =i+1; j<=array.length; j++){
+            //console.log(j)
+            if(array[i]===array[j]){
+                count=count +1;
+              array.splice(i,1);
+            }
+            
+        }
+    }
+    return array
+}
+
+console.log(x(value))
